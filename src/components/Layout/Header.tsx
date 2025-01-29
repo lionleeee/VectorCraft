14d5de3368d05f7db840c831ea88dd3875fb6214
lionleee <lee97hun3@gmail.com>
2025-01-29 17:50:29 +0900

    style : 헤더 버튼 수정

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,14 @@
+import { Button } from "../common/Button/Button";
+
 export const Header = () => {
   return (
     <header className="bg-black text-white px-4 py-2">
       <div className="flex justify-between items-center ">
         <label>VectorCraft</label>
         <div className="flex gap-4 text-sm">
-          <button>설정 내보내기</button>
-          <button>설정 불러오기</button>
-          <button>PNG다운로드</button>
+          <Button>설정 내보내기</Button>
+          <Button>설정 불러오기</Button>
+          <Button>PNG다운로드</Button>
         </div>
       </div>
     </header>
